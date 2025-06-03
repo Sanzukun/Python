4911ce02d67be294f0c50b13535b54d1df16e4f4
Mausham Kumar <Maushamroy143@gmail.com>
2025-06-03 09:53:47 +0530

    Day 2 Variable and Data types

---
+++ Day 02/Variable and Data Type.py
@@ -0,0 +1,47 @@
+# Variable 
+
+Name = "Mausham"
+work = "Mausham Sone chalo"
+
+print(Name)
+print(work)
+
+# Start with a letter or underscore (_)
+name = 'mausham'
+_count = 1
+print(_count)
+
+# Can contain letters (a-z, A-Z), digits (0–9), and underscores (_) only
+student_1 = "mausham"
+stuen4m = "Shail"
+
+# Case-sensitive
+Age = '23'
+age = '32'
+
+
+# Cannot start with a number.
+# 9Mausham ❌ 
+
+# No spaces or special characters like @, $, %, etc.
+# Mau@143 ❌ 
+
+# Cannot be a reserved keyword
+# Example: class, for, if are invalid as variable names.
+
+# ❌ Example: if = 5 → Invalid
+
+# No spaces allowed
+
+# Use _ instead of space
+
+# ❌ Example: total marks → Invalid
+
+# ✔ Example: total_marks → Valid
+
+
+# Data Types 
+age = int(input("Enter your age:"))
+salary = float(input("Enter your salary"))
+name = str(input("Enter your Name"))
+python = bool(input("Do you know python or not"))
